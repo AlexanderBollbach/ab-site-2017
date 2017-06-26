b563bed2709e2ff65bf147e0878f84019af26051
Alexander.Bollbach <alexander.bollbach@1stdibs.com>
2017-06-26 11:54:01 -0400

    mongo code to fetch docs from collection.  Also enable CORS.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,47 @@
 const express = require('express')
 const app = express()
+
+const database = "ab-website"
+const mongoUser = "alexanderbollbach"
+const mongopassword = "rhapsody"
+
+// var cors = require('cors');
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+var DB;
+
+
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect(`mongodb://${mongoUser}:${mongopassword}@ds135669.mlab.com:35669/ab-website`, function(err, db) {
+  if (!err) {
+    console.log("We are connected");
+    DB = db;
+  } else {
+    console.log(err)
+  }
+});
 app.get('/', (req, res) => {
-  res.send('HEY!')
+
+
+res.setHeader('Content-Type', 'application/json');
+ var collection = DB.collection('projects');
+
+  collection.find().toArray(function(err, items) {
+
+
+res.send(items);
+
+  });
+
+
+
 })
-app.listen(80, () => console.log('Server running on port 3000'))
+app.listen(8080, () => console.log('Server running on port 3000'))
